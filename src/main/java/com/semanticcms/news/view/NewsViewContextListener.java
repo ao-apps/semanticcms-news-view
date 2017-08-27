ede34e5a5021a94b192d76aaa6c758193bd9a691
AO Industries, Inc. <support@aoindustries.com>
2017-08-27 13:03:04 -0500

    Separating model from rendering.

--- src/main/java/com/semanticcms/news/view/NewsViewContextListener.java
+++ src/main/java/com/semanticcms/news/view/NewsViewContextListener.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-news-view - SemanticCMS view of all news in the current page and all children.
- * Copyright (C) 2016  AO Industries, Inc.
+ * Copyright (C) 2016, 2017  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,17 +22,17 @@
  */
 package com.semanticcms.news.view;
 
-import com.semanticcms.core.servlet.SemanticCMS;
+import com.semanticcms.core.renderer.html.HtmlRenderer;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-@WebListener("Registers the \"" + NewsView.VIEW_NAME + "\" view in SemanticCMS.")
+@WebListener("Registers the \"" + NewsView.VIEW_NAME + "\" view in HtmlRenderer.")
 public class NewsViewContextListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-		SemanticCMS.getInstance(event.getServletContext()).addView(new NewsView());
+		HtmlRenderer.getInstance(event.getServletContext()).addView(new NewsView());
 	}
 
 	@Override
