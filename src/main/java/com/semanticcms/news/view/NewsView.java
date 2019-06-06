be5aed25333cd50e7150ef4e8cab8d4dbf409a96
AO Industries, Inc <support@aoindustries.com>
2019-06-06 15:48:00 -0500

    Minimized diff versus 2.x branch

--- src/main/java/com/semanticcms/news/view/NewsView.java
+++ src/main/java/com/semanticcms/news/view/NewsView.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-news-view - SemanticCMS view of all news in the current page and all children.
- * Copyright (C) 2016, 2019  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2019  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
