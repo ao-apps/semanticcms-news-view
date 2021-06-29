2dc6b8e66534c35a959cd32083b7453817154ea8
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:45:09 -0500

    Now supports Java 9+ modules.

---
+++ book/src/main/java/com/semanticcms/news/view/book/ModularApidocsRedirect.java
@@ -0,0 +1,48 @@
+/*
+ * semanticcms-news-view - SemanticCMS view of all news in the current page and all children.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-news-view.
+ *
+ * semanticcms-news-view is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-news-view is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-news-view.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.news.view.book;
+
+import com.aoapps.net.URIParametersUtils;
+import com.aoapps.servlet.http.HttpServletUtil;
+import java.io.IOException;
+import java.util.Objects;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/news/view/apidocs/com/*")
+public class ModularApidocsRedirect extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpServletUtil.sendRedirect(
+			HttpServletResponse.SC_MOVED_PERMANENTLY, req, resp,
+			"/news/view/apidocs/com.semanticcms.news.view/com" + Objects.toString(req.getPathInfo(), ""),
+			URIParametersUtils.of(req.getQueryString()), true, false
+		);
+	}
+}
